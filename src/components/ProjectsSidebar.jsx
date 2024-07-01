b0a6fbd5efdee7c7eb059272f3a01f3a5c76c0a9
umgu <wnsghk112@naver.com>
2024-07-01 23:26:50 +0900

    너비의 1/3 class 추가

--- src/components/ProjectsSidebar.jsx
+++ src/components/ProjectsSidebar.jsx
@@ -1,9 +1,11 @@
 export default function ProjectsSidebar() {
-    return <aside>
-        <h2>my Projects</h2>
-        <div>
-            <button>+ Add Project</button>
-        </div>
-        <ul></ul>
+  return (
+    <aside className="w-1/3">
+      <h2>my Projects</h2>
+      <div>
+        <button>+ Add Project</button>
+      </div>
+      <ul></ul>
     </aside>
+  );
 }
