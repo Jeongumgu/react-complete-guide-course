1928dda33eafaa2a87a2d67251b8eaa0359b1d70
umgu <wnsghk112@naver.com>
2024-07-03 23:19:26 +0900

    x좌표 너비 추가

--- src/components/ProjectsSidebar.jsx
+++ src/components/ProjectsSidebar.jsx
@@ -1,6 +1,6 @@
 export default function ProjectsSidebar() {
   return (
-    <aside className="w-1/3">
+    <aside className="w-1/3 px-8">
       <h2>my Projects</h2>
       <div>
         <button>+ Add Project</button>
